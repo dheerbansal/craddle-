931b91b9bd166d4a8b02fac5fa84b585623f436e
dheerbansal <67144062+dheerbansal@users.noreply.github.com>
2020-09-05 11:33:28 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -1,7 +1,7 @@
 class Bob{
     constructor(x,y,radius){
         var options={
-            isStatic:true,
+            isStatic:false,
             restitution:0.3,
             friction:1.3,
             density:0.9
--- sketch.js
+++ sketch.js
@@ -13,14 +13,23 @@ function setup() {
     engine = Engine.create();
 	world = engine.world;
 //making the ball's using Bob class
-	ball1 = new Bob(300,600,60);
+	ball1 = new Bob(300,500,60);
 	ball2 = new Bob(358,600,60);
 	ball3 = new Bob(416,600,60);
 	ball4 = new Bob(474,600,60);
   ball5 = new Bob(532,600,60);
   
   //making the ground using ground clas
-  ground = new Ground(390,150,300,25);
+  ground = new Ground(700,150,700,25);
+
+  var options= {
+   bodyA:ball1.body,
+   bodyB:ground.body,
+   length: 400,
+   stiffness:0.9
+}
+var constraint = Constraint.create(options);
+  World.add(world,constraint)
 
   var render = Render.create({
 		element:document.body,
@@ -32,7 +41,7 @@ function setup() {
 		}
 	});
   //creating rope using rope class
-  rope1 = new Rope(ball1.body,ground.body,-ball1.radius*2,0);
+ // rope1 = new Rope(ball1.body,ground.body,-ball1.radius*2,0);
 
 	Engine.run(engine)
   
@@ -42,6 +51,8 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(255);
+
+  
  
   //displaying the objects
   ball1.display();
@@ -50,8 +61,9 @@ function draw() {
   ball4.display();
   ball5.display();
   ground.display();
-  rope1.display();
+  //rope1.display();
  // drawSprites();
+
  
 }
 
@@ -68,6 +80,7 @@ function drawLine(constraint){
 function mouseDragged(){
   Matter.Body.setPosition(ball1.body,{x:mouseX,y:mouseY})
 }
+
 
 function mouseReleased(){
   rope.fly();
